5dd32d89f52ba085c745fe133fdea43722d662f9
Paul Bellamy <paul@connectedenvironments.com>
2010-10-05 11:14:03 +0100

    wrote datastreamGet

--- jasmine/spec/PachubeAPI_spec.js
+++ jasmine/spec/PachubeAPI_spec.js
@@ -1,18 +1,95 @@
-describe("PachubeAPI", function(){
-  beforeEach(function(){
-    var api = new PachubeAPI();
+describe("PachubeAPI", function() {
+  var api;
+
+  beforeEach(function() {
+    api = new PachubeAPI();
   });
 
-  it("should be defined", function(){
-    expect(api != undefined);
+  it("should be defined", function() {
+    expect(api).not.toEqual(undefined);
   });
 
-  it("should have a datastream_get function", function(){
-    expect(api.datastreamGet != undefined);
-  });
-
-  it("should take an optional api_key and set it when creating the api", function(){
+  it("should take an optional api_key and set it when creating the api", function() {
     api = new PachubeAPI({ api_key: "myApiKey"});
-    expect(api.settings.api_key == "myApiKey");
+    expect(api.settings.api_key).toEqual("myApiKey");
+  });
+
+  it("should have a datastreamGet function", function() {
+    expect(api.datastreamGet).not.toEqual(undefined);
+  });
+
+  describe("datastreamGet", function() {
+    var api;
+
+    beforeEach(function() {
+      api = new PachubeAPI();
+    });
+
+    it("should call an ajax request to api.pachube.com", function() {
+      spyOn($, 'ajax');
+      var callback = function() {};
+      var options = {
+        resource: "myResource"
+      , api_key: "myApiKey"
+      , interval: 500
+      , start: new Date()
+      , end: new Date()
+      , per_page: 2000
+      , callback: callback
+      };
+
+      runs(function() {
+        api.datastreamGet(options);
+      });
+
+      waits(500);
+
+      runs(function() {
+        expect($.ajax).toHaveBeenCalledWith({
+          url: 'http://api.pachube.com/v2/' + options.resource + '.json?key=' + options.api_key + '&interval=' + options.interval + '&start=' + options.start.toISOString() + '&end=' + options.end.toISOString() + '&per_page=' + options.per_page
+        , success: options.callback
+        , dataType: 'jsonp'
+        });
+      });
+    });
+
+    it("should use the default api_key if we don't provide one at call time", function() {
+      api = new PachubeAPI({api_key: "oldApiKey"});
+
+      spyOn($, 'ajax');
+      var callback = function() {};
+      var options = {
+        resource: "myResource"
+      , interval: 500
+      , start: new Date()
+      , end: new Date()
+      , per_page: 2000
+      , callback: callback
+      };
+
+      api.datastreamGet(options);
+
+      expect($.ajax).toHaveBeenCalledWith({
+        url: 'http://api.pachube.com/v2/' + options.resource + '.json?key=' + api.settings.api_key + '&interval=' + options.interval + '&start=' + options.start.toISOString() + '&end=' + options.end.toISOString() + '&per_page=' + options.per_page
+      , success: options.callback
+      , dataType: 'jsonp'
+      });
+    });
+
+    it("should only *require* a resource", function() {
+      api = new PachubeAPI("myApiKey");
+      spyOn($, 'ajax');
+      var options = {
+        resource: "myResource"
+      };
+
+      api.datastreamGet(options);
+
+      expect($.ajax).toHaveBeenCalledWith({
+        url: 'http://api.pachube.com/v2/' + options.resource + '.json?key=' + api.settings.api_key
+      , dataType: 'jsonp'
+      });
+    });
   });
 });
+
--- jasmine/src/PachubeAPI.js
+++ jasmine/src/PachubeAPI.js
@@ -1,16 +1,42 @@
 function PachubeAPI(options) {
-  // Magic Incantation to enforce new
+  // Magic Incantation to enforce instantiation
   if (! (this instanceof arguments.callee)) {
     return new arguments.callee(arguments);
   }
 
   var self = this;
 
+  self.settings = {};
   if (options != undefined) {
     self.settings = {
       api_key: options.api_key
     };
   }
+  self.settings.api_url = 'http://api.pachube.com/v2/';
 
-  this.datastreamGet = function() {};
 }
+
+PachubeAPI.prototype.datastreamGet = function(options) {
+  var self = this;
+
+  var url = {
+    base: self.settings.api_url
+  , resource: options.resource
+  , format: '.json'
+  , key: '?key=' + (options.api_key || self.settings.api_key)
+  , interval: ''
+  , start: ''
+  , end: ''
+  , per_page: ''
+  };
+  if (options.interval != undefined) { url.interval = '&interval=' + options.interval }
+  if (options.start != undefined) { url.start = '&start=' + options.start.toISOString() }
+  if (options.end != undefined) { url.end = '&end=' + options.end.toISOString() }
+  if (options.per_page != undefined) { url.per_page = '&per_page=' + options.per_page }
+
+  $.ajax({
+    url: url.base + url.resource + url.format + url.key + url.interval + url.start + url.end + url.per_page
+  , success: options.callback
+  , dataType: 'jsonp'
+  });
+};
