3fc2941278212214c57ad046c0532abdcc65a24e
Paul Bellamy <paul@connectedenvironments.com>
2010-10-08 15:03:28 +0100

    added tests for timespan/interval setting and data array existence

--- jasmine/lib/PachubeGraph.js
+++ jasmine/lib/PachubeGraph.js
@@ -16,6 +16,33 @@ function PachubeGraph(element) {
     , rolling: false
     , update: false
     };
+
+    self.set_timespan_and_interval();
+
+    // Where we will store fetched data
+    self.data = new Array();
+  }
+
+  self.set_timespan_and_interval = function() {
+    switch(self.element.attr('timespan')) {
+      case "3 months":
+        self.settings.timespan = 7776000000;
+        self.settings.interval = 86400;
+        break;
+      case "4 days":
+        self.settings.timespan = 345600000;
+        self.settings.interval = 3600;
+        break;
+      case "last hour":
+        self.settings.timespan = 3600000;
+        self.settings.interval = 60;
+        break;
+      default:
+      case "24 hours": // default
+        self.settings.timespan = 86400000;
+        self.settings.interval = 900;
+        break;
+    }
   }
 
   self.init(element);
--- jasmine/spec/PachubeGraph_spec.js
+++ jasmine/spec/PachubeGraph_spec.js
@@ -28,4 +28,40 @@ describe("PachubeGraph", function() {
     expect(minimal[0].graph.settings.rolling).toEqual(false);
     expect(minimal[0].graph.settings.update).toEqual(false);
   });
+
+  it("should parse a timespan values and set timespan and interval accordingly", function() {
+    minimal.pachubeGraph(); // defaults (should equal twenty_four_hours)
+    last_hour.pachubeGraph();
+    twenty_four_hours.pachubeGraph();
+    four_days.pachubeGraph;
+    three_months.pachubeGraph();
+
+    expect(minimal[0].graph.settings.timespan).toEqual(86400000);
+    expect(minimal[0].graph.settings.interval).toEqual(900);
+    expect(last_hour[0].graph.settings.timespan).toEqual(3600000);
+    expect(last_hour[0].graph.settings.interval).toEqual(60);
+    expect(twenty_four_hours[0].graph.settings.timespan).toEqual(86400000);
+    expect(twenty_four_hours[0].graph.settings.interval).toEqual(900);
+    expect(four_days[0].graph.settings.timespan).toEqual(345600000);
+    expect(four_days[0].graph.settings.interval).toEqual(3600);
+    expect(three_months[0].graph.settings.timespan).toEqual(7776000000);
+    expect(three_months[0].graph.settings.interval).toEqual(864000);
+  });
+
+  it("should have a 'data' parameter for storing fetched data", function() {
+    minimal.pachubeGraph();
+
+    expect(minimal[0].graph.data).not.toEqual(undefined);
+  });
+
+  it("should send call the api to fetch the initial data", function() {
+    spyOn(pachubeAPI, datastreamGet);
+
+    minimal.pachubeGraph();
+
+    expect(pachubeAPI.datastreamGet).toHaveBeenCalledWith({
+      resource: minimal[0].graph.settings.resource
+    , api_key: minimal[0].graph.settings.api_key
+    });
+  });
 });
