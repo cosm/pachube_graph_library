a5e65f605acb7eb136853b88ad238c5b3fe4ffb5
Paul Bellamy <paul.a.bellamy@gmail.com>
2011-04-07 17:07:03 +0000

    Making it work with storage and on beta.apps.pachube.com

--- js/default.js
+++ js/default.js
@@ -14,14 +14,18 @@ var urlParams = {};
 })();
 
 $(function() {
+  var apps_url = 'http://beta.apps.pachube.com'
+  var script_url = 'http://beta.apps.pachube.com/embeddable_graphs';
+  var token = '';
+  var data = {system: {}, user: {}};
+
   function initializeForm(evt) {
     // This will populate all our text fields by url param
     for (var key in urlParams) {
       var element = $('form input:text[name="' + key + '"]');
       if (element != []) {
         element.val(urlParams[key]);
-      }
-    }
+      }    }
 
     // Some special fields we should try to populate
     if (urlParams['rolling'] == 'on') { $('form input[name="rolling"]').attr({'checked':true}); }
@@ -29,30 +33,89 @@ $(function() {
     if (urlParams['timespan'] != undefined) {
       $('form select[name="timespan"]').val(urlParams['timespan']);
     }
+
+    // if we got passed a token from the appstore fetch data for it
+    if (urlParams["token"] != undefined) {
+      token = urlParams["token"];
+      fetchUserData(token);
+    } else {
+      updateSource();
+    }
   }
 
-  function submitForm(evt) {
-    var values = {};
-    $.each($('form').serializeArray(), function(i, field) {
-      values[field.name] = field.value || urlParams[field.name];
-    });
+  function fetchUserData(token) {
+    $('form fieldset#data').hide();
+    $.get(apps_url + "/conf/" + token, function(response) {
+      data = response;
+      $('form input[name="id"]').val(data.system.get[0].feed_id);
+      $('form input[name="stream_id"]').val(data.system.get[0].datastream_id);
+      $('form input[name="key"]').val(data.system.get[0].key);
 
-    var link_url = 'http://paulbellamy.com/pachube_graph_library?';
-    var url_values = [];
-    for (var key in values) {
-      if (values[key] != undefined && values[key] != '') {
-        url_values.push(escape(key) + '=' + escape(values[key]));
+      for (var i in data.user) {
+        if (data.user.hasOwnProperty(i) && urlParams[i] == undefined) {
+          $('form [name="'+i+'"]').val(data.user[i]);
+        }
+      }
+
+      // A few special fields we need to set up
+      if (data.user['rolling'] == 'on') { $('form [name="rolling"]').attr({'checked':true}); }
+      if (data.user['update']  == 'on') { $('form [name="update"]').attr({'checked':true});  }
+
+      updateSource();
+    });
+  }
+
+  function saveUserData() {
+    $.ajax({ url: apps_url + '/conf/'+token+'/user'
+           , type: 'PUT'
+           , processData: false
+           , data: JSON.stringify(data.user)
+           });
+  }
+
+  function readDataFromForm() {
+    var values = {};
+
+    $.each($('form').serializeArray(), function(i, field) {
+      if (field.value == '') { field.value = undefined; }
+      values[field.name] = field.value || urlParams[field.name];
+      data.user[field.name] = values[field.name] || data.user[field.name];
+    });
+    var k = Object.keys(data.user);
+    for (var i in k) {
+      var key = k[i];
+      if (data.user.hasOwnProperty(key) && values[key] == undefined) {
+        delete data.user[key];
       }
     }
-    link_url += url_values.join('&');
 
+    // Set some defaults
     values.id = values.id || "504";
     values.stream_id = values.stream_id || "1";
     values.key = values.key || '1iObDqRLQTi6Z3L-Gf7rKBJfSfSvrwFsmE83KrpYtCY';
     values.width = values.width || '420px';
     values.height = values.height || '240px';
     values.background = values.background || '#F0F0F0';
-    var head_html = '<script type="text/javascript" src="http://paulbellamy.com/pachube_graph_library/lib/PachubeLoader.js"></script>';
+
+    return values;
+  }
+
+  function updateSource() {
+    var values = readDataFromForm();
+
+    // Save our values to the server
+    saveUserData();
+
+    var link_url = script_url + '?';
+    var url_values = [];
+    for (var key in data.user) {
+      if (values[key] != undefined && values[key] != '') {
+        url_values.push(escape(key) + '=' + escape(values[key]));
+      }
+    }
+    link_url += url_values.join('&');
+
+    var head_html = '<script type="text/javascript" src="'+script_url+'/lib/PachubeLoader.js"></script>';
     var options = { "timespan": values['timespan'] || '24 hours'
                   , "rolling":  values['rolling'] == 'on'
                   , "update":   values['update'] == 'on'
@@ -60,6 +123,7 @@ $(function() {
                   , "line-color":        values['line-color'] || "#FF0066"
                   , "grid-color":        values['grid-color'] || "#EFEFEF"
                   , "border-color":      values['border-color'] || "#9D9D9D"
+
                   , "text-color":        values['text-color'] || "#555555"
                   };
 
@@ -106,6 +170,10 @@ $(function() {
         );
   }
 
+  function submitForm(evt) {
+    updateSource();
+  }
+
   $('#ourForm :input').bind('change', function(evt) {
     submitForm(evt);
     return false;
@@ -118,5 +186,4 @@ $(function() {
 
   // Set stuff up
   initializeForm();
-  submitForm();
 });
